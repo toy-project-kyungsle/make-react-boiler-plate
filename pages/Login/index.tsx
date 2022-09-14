f52945ca1bf94106048728031ee8027a1fbac1c4
‘keinn51’ <soogood97@gmail.com>
2022-09-14 17:14:15 +0900

    test

--- pages/Login/index.tsx
+++ pages/Login/index.tsx
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>로그인</div>
+    <>
+      <div>
+        <div className='header'>
+          <div className='header-logo'></div>
+          <div className='header-auth_box'></div>
+        </div>
+      </div>
+      <div>
+        <div className='section'>
+          <div className='section-category'></div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
